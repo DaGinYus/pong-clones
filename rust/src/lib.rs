81da9ab89bdf5ae05867fea50c8c7a4966e23df4
Matthew <matthew@adveca.com>
2024-03-21 15:05:01 -0700

    prototyping seven segment, refactored polygon drawing logic

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,6 +3,10 @@
 // a 'true' clone of the popular game, Pong
 // ref: https://www.pong-story.com/LAWN_TENNIS.pdf
 
+use std::convert::TryInto;
+use godot::prelude::*;
+use godot::engine::{Polygon2D, IPolygon2D, Area2D, IArea2D};
+
 // pixel conversion information
 // the 'resolution' of the video signal was 455x262 clock signals (60Hz VSYNC)
 // the HBLANK signal was 81 CLKs long, for an active video time of 374 CLKs
@@ -12,10 +16,6 @@
 // say we want to define the active area to be 640x480 (VGA)
 // then 640px / 53.36us = 12 px/us = 1.68 px/1H 
 //      480px / 62.48ms = 1.89 px/ms = 1.95 px/1V
-
-use godot::prelude::*;
-use godot::engine::{Polygon2D, Area2D, IArea2D};
-
 // these are hardcoded, maybe consider making these dynamic based on viewport settings
 // a singleton containing game constants could be helpful here
 const VIEWPORT_HEIGHT: i32 = 480;
@@ -65,8 +65,8 @@ impl INode for Main {
     fn init(base: Base<Node>) -> Self {
         Self {
             net: Net::new_alloc(),
-            paddle_l: Paddle::from_side(PaddleSide::Left),
-            paddle_r: Paddle::from_side(PaddleSide::Right),
+            paddle_l: Paddle::from_side(PlayerSide::Left),
+            paddle_r: Paddle::from_side(PlayerSide::Right),
             base
         } 
     }
@@ -76,10 +76,51 @@ impl INode for Main {
         let paddle_l_clone = self.paddle_l.clone();
         let paddle_r_clone = self.paddle_r.clone();
         self.base_mut().add_child(net_clone.clone().upcast());
-        self.net.bind_mut().draw();
         self.base_mut().add_child(paddle_l_clone.upcast());
         self.base_mut().add_child(paddle_r_clone.upcast());
     }
+}
+
+#[derive(Clone)]
+struct Rect<T> {
+    x: T,
+    y: T,
+    w: T,
+    h: T,
+}
+
+impl Into<Rect<f32>> for Rect<i32> {
+    fn into(self) -> Rect<f32> {
+        Rect {
+            x: self.x as f32,
+            y: self.y as f32,
+            w: self.w as f32,
+            h: self.h as f32,
+        }
+    }
+}
+
+fn polygon2d_add_rect(polygon: &mut Gd<Polygon2D>, rect: Rect<i32>, redraw: bool) {
+    let mut vertices = match redraw {
+        true => PackedVector2Array::new(),
+        false => polygon.get_polygon(),
+    };
+    let rect_f: Rect<f32> = rect.clone().into();
+    vertices.push(Vector2::new(rect_f.x, rect_f.y));
+    vertices.push(Vector2::new(rect_f.x+rect_f.w, rect_f.y));
+    vertices.push(Vector2::new(rect_f.x+rect_f.w, rect_f.y+rect_f.h));
+    vertices.push(Vector2::new(rect_f.x, rect_f.y+rect_f.h));
+    polygon.set_polygon(vertices);
+}
+
+fn polygon2d_set_indices(polygon: &mut Gd<Polygon2D>) {
+    let mut polygon_indices = Array::<Variant>::new();
+    let num_vertices = polygon.get_polygon().len();
+    for i in 0..num_vertices/4 {
+        let base = 4*i as i32;
+        polygon_indices.push(PackedInt32Array::from(&[base, base+1, base+2, base+3]).to_variant());
+    }
+    polygon.set_polygons(polygon_indices);
 }
 
 #[derive(GodotClass)]
@@ -88,36 +129,33 @@ struct Net {
     base: Base<Polygon2D>
 }
 
+#[godot_api]
+impl IPolygon2D for Net {
+    fn ready(&mut self) {
+        self.draw();
+    }
+}
+
 impl Net {
     // the net is triggered at 256H from the HRST signal
     // the net is dependent on a 4V signal for the segments, and is only one pulse wide
     // this means the net should be drawn with roughly 2x8 segments 8px apart
     fn draw(&mut self) {
-        let net_left_edge = hclk_to_xpos(256) as f32;
-        let mut polygons = PackedVector2Array::new();
-        let mut polygon_indices = Array::<Variant>::new();
+        let net_left_edge = hclk_to_xpos(256);
         let net_segment_spacing: usize = vclk_to_interval(8).try_into().unwrap();
 
-        let net_width = hclk_to_interval(1) as f32;
-        let net_height = vclk_to_interval(4) as f32;
+        let net_width = hclk_to_interval(1);
+        let net_height = vclk_to_interval(4);
         for i in (0..VIEWPORT_HEIGHT).step_by(net_segment_spacing) {
-            let to_float_i = i as f32;
-            polygons.push(Vector2::new(net_left_edge, to_float_i));
-            polygons.push(Vector2::new(net_left_edge+net_width, to_float_i));
-            polygons.push(Vector2::new(net_left_edge+net_width, to_float_i+net_height));
-            polygons.push(Vector2::new(net_left_edge, to_float_i+net_height));
+            let i_int = i as i32;
+            let rect = Rect { x: net_left_edge, y: i_int, w: net_width, h: net_height };
+            polygon2d_add_rect(&mut self.base_mut(), rect, false);
         }
-        let num_vertices = polygons.len();
-        for i in 0..num_vertices/4 {
-            let base = 4*i as i32;
-            polygon_indices.push(PackedInt32Array::from(&[base, base+1, base+2, base+3]).to_variant());
-        }
-        self.base_mut().set_polygon(polygons);
-        self.base_mut().set_polygons(polygon_indices);
+        polygon2d_set_indices(&mut self.base_mut());
     }
 }
 
-enum PaddleSide {
+enum PlayerSide {
     Left,
     Right
 }
@@ -126,7 +164,7 @@ enum PaddleSide {
 #[class(base=Area2D)]
 struct Paddle {
     ypos: i32,
-    side: PaddleSide,
+    side: PlayerSide,
     polygon: Gd<Polygon2D>,
     base: Base<Area2D>
 }
@@ -137,7 +175,7 @@ impl IArea2D for Paddle {
         let init_y = vclk_to_ypos(128);
         Self {
             ypos: init_y,
-            side: PaddleSide::Left,
+            side: PlayerSide::Left,
             polygon: Polygon2D::new_alloc(),
             base
         }
@@ -151,11 +189,11 @@ impl IArea2D for Paddle {
     fn process(&mut self, _delta: f64) {
         let input = Input::singleton();
         match self.side {
-            PaddleSide::Left => {
+            PlayerSide::Left => {
                 if input.is_action_pressed("up_l".into()) { self.move_up() }
                 if input.is_action_pressed("dn_l".into()) { self.move_down()}
             },
-            PaddleSide::Right => {
+            PlayerSide::Right => {
                 if input.is_action_pressed("up_r".into()) { self.move_up() }
                 if input.is_action_pressed("dn_r".into()) { self.move_down() }
             }
@@ -165,7 +203,7 @@ impl IArea2D for Paddle {
 }
 
 impl Paddle {
-    fn from_side(side: PaddleSide) -> Gd<Self> {
+    fn from_side(side: PlayerSide) -> Gd<Self> {
         let init_y = vclk_to_ypos(128);
         Gd::from_init_fn(|base| {
             Self {
@@ -182,19 +220,15 @@ impl Paddle {
     // the ball's vertical velocity is determined by which segment it hits
     // the new vertices are always pushed, this might be slow--consider only updating when y changes
     fn draw(&mut self) {
-        let mut vertices = PackedVector2Array::new();
         let xpos = match self.side {
-            PaddleSide::Left => hclk_to_xpos(128),
-            PaddleSide::Right => hclk_to_xpos(128+256),
-        } as f32;
-        let ypos = self.ypos as f32;
-        let bat_height = vclk_to_interval(15) as f32;
-        let bat_width = hclk_to_interval(4) as f32;
-        vertices.push(Vector2::new(xpos, ypos));
-        vertices.push(Vector2::new(xpos+bat_width, ypos));
-        vertices.push(Vector2::new(xpos+bat_width, ypos+bat_height));
-        vertices.push(Vector2::new(xpos, ypos+bat_height));
-        self.polygon.set_polygon(vertices);
+            PlayerSide::Left => hclk_to_xpos(128),
+            PlayerSide::Right => hclk_to_xpos(128+256),
+        };
+        let ypos = self.ypos;
+        let bat_height = vclk_to_interval(15);
+        let bat_width = hclk_to_interval(4);
+        let rect = Rect { x: xpos, y: ypos, w: bat_width, h: bat_height };
+        polygon2d_add_rect(&mut self.polygon, rect, true);
     }
 
     // the paddles actually could not move the entire range
@@ -219,6 +253,67 @@ impl Paddle {
             self.ypos = new_ypos
         } else {
             self.ypos = max_ypos
+        }
+    }
+}
+
+#[derive(GodotClass)]
+#[class(base=Node2D)]
+struct ScoreDisplay {
+    score: [i32; 2],
+    base: Base<Node2D>
+}
+
+#[godot_api]
+impl INode2D for ScoreDisplay {
+    fn init(base: Base<Node2D>) -> Self {
+        Self {
+            score: [0, 0],
+            base
+        }
+    }
+}
+
+impl ScoreDisplay {
+    //    _a_
+    // f |_g_| b
+    // e |___| c
+    //     d
+    // converts to an array of values [a, b, c, d, e, f, g]
+    fn n_to_seven_segment(n: i32) -> Option<[i8; 7]> {
+        match n {
+            0 => Some([1, 1, 1, 1, 1, 1, 0]),
+            1 => Some([0, 1, 1, 0, 0, 0, 0]),
+            2 => Some([1, 1, 0, 1, 1, 0, 1]),
+            3 => Some([1, 1, 1, 1, 0, 0, 1]),
+            4 => Some([0, 1, 1, 0, 0, 1, 1]),
+            5 => Some([1, 0, 1, 1, 0, 1, 1]),
+            6 => Some([1, 0, 1, 1, 1, 1, 1]),
+            7 => Some([1, 1, 1, 0, 0, 0, 0]),
+            8 => Some([1, 1, 1, 1, 1, 1, 1]),
+            9 => Some([1, 1, 1, 0, 0, 1, 1]),
+            _ => None
+        }
+    }
+
+    // the score windows were positioned 32V from the top of the screen
+    // for two digit scores, the numbers were 4H apart from each other
+    // the leftmost edge was at 144H, so the next leftmost would be at 160H
+    // for P2 on the right, the leftmost segment was at 336H and the second digit was at 352H
+    fn draw_seven_segment(&mut self) {
+        let offset_y = vclk_to_ypos(32);
+        for (player, score) in self.score.iter().enumerate() {
+            let ones_digit = score % 10;
+            let tens_digit = score / 10;
+            // trick to calculate offsets using the indices of the scores
+            let tens_offset_x = hclk_to_xpos(144) + (player as i32)*hclk_to_interval(192);
+            let ones_offset_x = tens_offset_x + hclk_to_interval(16);
+
+            // make a list of rects, then zip/map with the n_to_seven_segment and draw only if 1
+            let segments = todo!();
+
+            if tens_digit != 0 {
+            }
         }
     }
 }
